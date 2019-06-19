caa1a50b2c5657b4542e3a652dd6276741be6e15
Hans Meyer <Hans.Meyer@team-beverage.de>
2019-06-19 10:12:50 +0200

    Let users add products to catalogs

--- API/Resolvers/CatalogResolver.cs
+++ API/Resolvers/CatalogResolver.cs
@@ -1,6 +1,9 @@
 ﻿using HotChocolate;
 using HotChocolate.Types;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace graphql_showcase.API.Resolvers
 {
@@ -18,6 +21,20 @@ namespace graphql_showcase.API.Resolvers
         public string Name([Parent] Domain.Catalog catalog)
         {
             return catalog.Name;
+        }
+
+        public async Task<IList<Domain.Product>> Products([Parent] Domain.Catalog catalog,
+                                                          [Service] Domain.DataAccess.IProductRepository productRepository)
+        {
+            var products = new List<Domain.Product>();
+
+            foreach (var productID in catalog.Products)
+            {
+                var referencedProduct = await productRepository.GetProductById(productID);
+                products.Add(referencedProduct);
+            }
+
+            return products;
         }
     }
 }
